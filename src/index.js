d984814cfe93dd6aa24f11da0446251ea3d1bd7b
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-08-14 19:24:58 +0300

    add fetchImg

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/styles.css';
+import { fetchImages } from './js/fetchImg';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
---
+++ src/js/fetchImg.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export { fetchImages };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '29268456-6bc69d63a2108bddafd6e85b4';
+const FILTER_RESPONSE =
+  'image_type=photo&orientation=horizontal&safesearch=true';
+
+const fetchImages = async (query, page, perPage) => {
+  try {
+    const response = await axios.get(
+      `?key=${API_KEY}&q=${query}&page=${page}&per_page=${perPage}&${FILTER_RESPONSE}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
